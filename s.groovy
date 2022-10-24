6333b7d86f12ab628c5a27e63ca85d6eec3e0918
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-24 10:38:18 +0900

    added files

--- s.groovy
+++ s.groovy
@@ -1,4 +1,4 @@
-def folder(dslScript, folders) {
+def call(dslScript = "seed_folder.groovy", folders) {
     print "inside createfolders"
 
     jobDsl targets: dslScript, additionalParameters: [folders: folders]

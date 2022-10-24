3f5919740e38e63be638736fa9070e343dead910
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-25 01:18:55 +0900

    added files

--- s.groovy
+++
@@ -1,5 +0,0 @@
-d = [common:[region:'ap-northeast-1'], 
-awsCodePipeline:[codeCommitRepo:'shakti-test-repo', cfStack:'s3-bucket-cf', codePipeline:'shakti-test-nodejs'], 
-awsEventRule:[eventRule:'test']
-]
-println(d.awsCodePipeline.get("codeCommitRepo", "k"))

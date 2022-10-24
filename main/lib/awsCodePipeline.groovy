055bcf584c15b148a68515da7f022ca0eabcf768
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-25 00:36:26 +0900

    added files

--- main/lib/awsCodePipeline.groovy
+++ main/lib/awsCodePipeline.groovy
@@ -1,15 +1,17 @@
-params=[:]
-config.each{key, value ->
-    value.each{pipeline_name, pipeline_val -> 
-        params[pipeline_name] = [:]
-        if (pipeline_name != "common"){
-            params[pipeline_name].codeCommitRepo = pipeline_val.get("codeCommitRepo", "-")
-            params[pipeline_name].cfStack = pipeline_val.get("cfStack", "-")
-            params[pipeline_name].codePipeline = pipeline_val.get("codePipeline", "-")
-            params[pipeline_name].eventRule = pipeline_val.get("eventRule", "-")
+def call(pipeline_data){
+    params=[:]
+    config.each{key, value ->
+        value.each{pipeline_name, pipeline_val -> 
+            params[pipeline_name] = [:]
+            if (pipeline_name != "common"){
+                params[pipeline_name].codeCommitRepo = pipeline_val.get("codeCommitRepo", "-")
+                params[pipeline_name].cfStack = pipeline_val.get("cfStack", "-")
+                params[pipeline_name].codePipeline = pipeline_val.get("codePipeline", "-")
+                params[pipeline_name].eventRule = pipeline_val.get("eventRule", "-")
+            }
+            else if (pipeline_name == "common")
+                params[pipeline_name].region = pipeline_val.get("region", "-")
         }
-        else if (pipeline_name == "common")
-            params[pipeline_name].region = pipeline_val.get("region", "-")
-    }
-}  
-return params
+    }  
+    return params
+}

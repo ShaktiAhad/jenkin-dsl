8a440f681ce0db1a8ba11143f328abb504db6a32
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-25 01:23:17 +0900

    added files

--- main/lib/awsCodePipeline.groovy
+++ main/lib/awsCodePipeline.groovy
@@ -4,13 +4,13 @@ def call(pipeline_data){
         value.each{pipeline_name, pipeline_val -> 
             params[pipeline_name] = [:]
             if (pipeline_name != "common"){
-                params[pipeline_name].codeCommitRepo = pipeline_val.get("codeCommitRepo", "-")
-                params[pipeline_name].cfStack = pipeline_val.get("cfStack", "-")
-                params[pipeline_name].codePipeline = pipeline_val.get("codePipeline", "-")
-                params[pipeline_name].eventRule = pipeline_val.get("eventRule", "-")
+                params[pipeline_name].codeCommitRepo = pipeline_val.get("codeCommitRepo", "empty")
+                params[pipeline_name].cfStack = pipeline_val.get("cfStack", "empty")
+                params[pipeline_name].codePipeline = pipeline_val.get("codePipeline", "empty")
+                params[pipeline_name].eventRule = pipeline_val.get("eventRule", "empty")
             }
             else if (pipeline_name == "common")
-                params[pipeline_name].region = pipeline_val.get("region", "-")
+                params[pipeline_name].region = pipeline_val.get("region", "empty")
         }
     }  
     return params

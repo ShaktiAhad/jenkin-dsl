50c97259f2491efcf9b521a3ae0e5830b9e93133
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-25 14:16:47 +0900

    added files

--- vars/shReturnJson.groovy
+++ vars/shReturnJson.groovy
@@ -1,9 +1,9 @@
 import groovy.json.JsonSlurperClassic
 
 def call(aws_command){
-    // def credentialsFile = "/var/jenkins_home/credentials"
-    // withEnv(["AWS_SHARED_CREDENTIALS_FILE=${credentialsFile}"])
-    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+    // withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
+    def credentialsFile = "/var/jenkins_home/credentials"
+    withEnv(["AWS_SHARED_CREDENTIALS_FILE=${credentialsFile}"]){
         def result = shReturnStdout(aws_command)
         if (result.length() == 0){
             println (" ## No json output.")

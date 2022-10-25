004b0d4fe92a6b6e868e0002af6b125a1d295663
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-25 13:55:38 +0900

    added files

--- vars/awsUpdateCloudformationStack.groovy
+++ vars/awsUpdateCloudformationStack.groovy
@@ -1,4 +1,3 @@
 def call(region, stack, bucket="watashino-tesuto-bucketsu", params){
-    shReturnJson("aws s3api delete-bucket --bucket ${bucket} --region ${region}")
     return (shReturnJson("aws cloudformation update-stack --region ${region} --stack-name ${stack} --use-previous-template --parameters '${params}' --capabilities CAPABILITY_NAMED_IAM"))
 }

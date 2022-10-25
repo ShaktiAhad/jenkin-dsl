eb0a18a19121b495f97b109c74c961811d5af125
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-25 11:45:23 +0900

    added files

---
+++ s.groovy
@@ -0,0 +1,35 @@
+@Grab('org.yaml:snakeyaml:1.33')
+import org.yaml.snakeyaml.Yaml
+
+def content = new Yaml().load(("projects/a/config.yaml" as File).text)
+pipeline_data = content["conf"]
+
+
+params=[:]
+pipeline_data.each{key, value ->
+    value.each{pipeline_name, pipeline_val -> 
+        params[pipeline_name] = [:]
+        if (pipeline_name != "common"){
+            params[pipeline_name].codeCommitRepo = pipeline_val.get("codeCommitRepo", "empty")
+            params[pipeline_name].cfStack = pipeline_val.get("cfStack", "empty")
+            params[pipeline_name].codePipeline = pipeline_val.get("codePipeline", "empty")
+            params[pipeline_name].eventRule = pipeline_val.get("eventRule", "empty")
+            params[pipeline_name].region = pipeline_val.get("region", "empty")
+        }
+        else if (pipeline_name == "common")
+            params[pipeline_name].gitUrl = pipeline_val.get("gitUrl", "empty")
+            params[pipeline_name].branch = pipeline_val.get("branch", "empty")
+            params[pipeline_name].cred = pipeline_val.get("cred", "empty")
+    }
+}
+
+def d = [env:"test",
+         sd: "eds",
+         asd: "dss"]
+
+sd = []
+for (f in d){
+    sd.add(["ParameterKey = ${f.key}, ParameterValue = ${f.value}"])
+
+}
+println (sd)
--- vars/awsUpdateCloudformationStack.groovy
+++ vars/awsUpdateCloudformationStack.groovy
@@ -1,4 +1,4 @@
-def call(region, stack, bucket="watashino-tesuto-bucketsu"){
+def call(region, stack, bucket="watashino-tesuto-bucketsu", params){
     shReturnJson("aws s3api delete-bucket --bucket ${bucket} --region ${region}")
-    return (shReturnJson("aws cloudformation update-stack --region ${region} --stack-name ${stack} --use-previous-template --parameters ParameterKey=BucketPrefix,ParameterValue=${bucket} --capabilities CAPABILITY_NAMED_IAM"))
+    return (shReturnJson("aws cloudformation update-stack --region ${region} --stack-name ${stack} --use-previous-template --parameters [${params}, {"ParameterKey":"BucketPrefix", "ParameterValue": "${bucket}"}]--capabilities CAPABILITY_NAMED_IAM"))
 }

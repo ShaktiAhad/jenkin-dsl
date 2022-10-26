68e2e19a8b24428f66febaccd053ead4a1b76ec9
ShaktiAhad <ahadnoor80@gmail.com>
2022-10-26 14:58:19 +0900

    updated eventRule pipeline

--- main/lib/awsCodePipeline.groovy
+++ main/lib/awsCodePipeline.groovy
@@ -1,19 +1,19 @@
 def call(pipeline_data){
     params=[:]
     pipeline_data.each{key, value ->
-        value.each{pipeline_name, pipeline_val -> 
+        value.each{pipeline_name, pipeline_val ->
             params[pipeline_name] = [:]
-            if (pipeline_name != "common"){
+            if (pipeline_name != "common" && pipeline_name != "awsCodePipeline"){
                 params[pipeline_name].codeCommitRepo = pipeline_val.get("codeCommitRepo", "empty")
                 params[pipeline_name].cfStack = pipeline_val.get("cfStack", "empty")
                 params[pipeline_name].codePipeline = pipeline_val.get("codePipeline", "empty")
-                params[pipeline_name].eventRule = pipeline_val.get("eventRule", "empty")
                 params[pipeline_name].region = pipeline_val.get("region", "empty")
             }
-            else if (pipeline_name == "common")
+            else if (pipeline_name == "common"){
                 params[pipeline_name].gitUrl = pipeline_val.get("gitUrl", "empty")
                 params[pipeline_name].branch = pipeline_val.get("branch", "empty")
                 params[pipeline_name].cred = pipeline_val.get("cred", "empty")
+            }
         }
     }  
     return params
--- s.groovy
+++ s.groovy
@@ -1,1 +1,29 @@
 // This file is for testing purpose only.
+
+// @Grab('org.yaml:snakeyaml:1.33')
+// import org.yaml.snakeyaml.Yaml
+// def content = new Yaml().load(("${workspace}/projects/${project_name}/config.yaml" as File).text)
+// config = content["conf"]
+
+
+import groovy.yaml.YamlSlurper
+filename = "config.yaml"
+content = new YamlSlurper().parse(new File("projects/a/config.yaml"))
+pipeline_data = content["conf"]
+
+params=[:]
+pipeline_data.each{key, value ->
+    value.each{pipeline_name, pipeline_val -> 
+        params[pipeline_name] = [:]
+        if (pipeline_name != "common" && pipeline_name == "awsEventRule"){
+            params[pipeline_name].eventRule = pipeline_val.get("eventRule", "empty")
+            params[pipeline_name].region = pipeline_val.get("region", "empty")
+        }
+        else if (pipeline_name == "common"){
+            params[pipeline_name].gitUrl = pipeline_val.get("gitUrl", "empty")
+            params[pipeline_name].branch = pipeline_val.get("branch", "empty")
+            params[pipeline_name].cred = pipeline_val.get("cred", "empty")
+        }
+    }
+} 
+println (params)

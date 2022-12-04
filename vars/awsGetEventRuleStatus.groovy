4a457f843d041cec6e5fe06c114142eb53ca6e55
Ahad Shakti <ahadshakti@Ahads-MacBook-Pro.local>
2022-12-05 03:11:47 +0900

    updated code

--- vars/awsGetEventRuleStatus.groovy
+++ vars/awsGetEventRuleStatus.groovy
@@ -1,4 +1,4 @@
 def call(region, rule){
     def awsGetEventStatus = awsDescribeEventRule(region, rule)
-    println ("Rule_name: ${awsGetEventStatus["Name"]}, Status: ${awsGetEventStatus["State"]}")
+    return ("Rule_name: ${awsGetEventStatus["Name"]}, Status: ${awsGetEventStatus["State"]}")
 }

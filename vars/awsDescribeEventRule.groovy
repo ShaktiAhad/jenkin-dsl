eeef4eba4e449a9b4bc22c6b380f65a42c3405d2
ShaktiAhad <ahadnoor80@gmail.com>
2023-01-25 11:51:52 +0900

    fixed eventRule

--- vars/awsDescribeEventRule.groovy
+++ vars/awsDescribeEventRule.groovy
@@ -1,4 +1,4 @@
 def call(region, rule){
-    awsDescribeEventRule = awsCommand("events describe-rule --region ${region} --name ${rule}")
-    println ("Rule_name: ${awsDescribeEventRule["Name"]}, Status: ${awsDescribeEventRule["State"]}")
+    awsDescribeRule = awsCommand("events describe-rule --region ${region} --name ${rule}")
+    println ("Rule_name: ${awsDescribeRule["Name"]}, Status: ${awsDescribeRule["State"]}")
 }

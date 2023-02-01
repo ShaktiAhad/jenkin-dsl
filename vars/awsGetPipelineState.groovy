8bb01fa204a2be6606b28fe8c753bc8f91b35806
ShaktiAhad <ahadnoor80@gmail.com>
2023-02-01 12:50:43 +0900

    fix awsGetPipelineState

--- vars/awsGetPipelineState.groovy
+++ vars/awsGetPipelineState.groovy
@@ -3,12 +3,7 @@ def call(region, pipeline, pipelineId){
     for (stage in get_pipeline_state["stageStates"]){
         if (stage["latestExecution"]["pipelineExecutionId"] == pipelineId){
             for (action in stage["actionStates"]){
-                if (action.containsKey('latestExecution')){
-                    println("stageName: ${stage["stageName"]}, actionName: ${action["actionName"]}, status: ${action['latestExecution']["status"]}")
-                }
-                else{
-                    println("stageName: ${stage["stageName"]}, actionName: ${action["actionName"]}, status: null")
-                }
+                println("stageName: ${stage["stageName"]}, actionName: ${action["actionName"]}, status: ${action['latestExecution']["status"]}")
             }
         }
     }
